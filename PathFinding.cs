9b718ffa033e4d6e33181b82382a2d32bc4193a1
Pedro Matos <pedrohugorm@gmail.com>
2023-01-24 01:10:22 -0600

    First Commit

---
+++ PathFinding.cs
@@ -0,0 +1,58 @@
+﻿namespace DUCalculator;
+
+using System.Linq;
+using System.Numerics;
+
+public static class PathFinding
+{
+    /// <summary>
+    /// Brute Force Algo to Get the Shortest Path
+    /// </summary>
+    /// <param name="startPoint"></param>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    public static IEnumerable<Vector3> GetShortestPath(Vector3 startPoint, Vector3[] points)
+    {
+        // Create a copy of the input list of points
+        var remainingPoints = (Vector3[])points.Clone();
+
+        // Initialize an empty list to store the final path
+        var finalPath = new Vector3[points.Length + 1];
+
+        // The current point starts as the start point
+        var currentPoint = startPoint;
+
+        // add the start point to the finalPath
+        finalPath[0] = startPoint;
+
+        // While there are still points in the list
+        for (var i = 1; i < finalPath.Length; i++)
+        {
+            // Find the closest point to the current point
+            var closestDistance = float.MaxValue;
+            var closestPoint = new Vector3();
+            
+            foreach (var point in remainingPoints)
+            {
+                var distance = Vector3.Subtract(currentPoint, point);
+                var length = Vector3.Dot(distance,distance);
+                if (length < closestDistance)
+                {
+                    closestDistance = length;
+                    closestPoint = point;
+                }
+            }
+
+            // Add the closest point to the final path
+            finalPath[i] = closestPoint;
+
+            // Remove the closest point from the remaining points list
+            remainingPoints = remainingPoints.Where(val => val != closestPoint).ToArray();
+
+            // Update the current point
+            currentPoint = closestPoint;
+        }
+
+        return finalPath;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace DUCalculator;
+
+public partial class Program
+{
+    public static void Main(params string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("Provide a File Path to Read for the first parameter and a start position as the second parameter");
+            return;
+        }
+        
+        var filePath = args[0];
+        var startPosition = args[1].PositionToVector3();
+
+        var lines = File.ReadAllLines(filePath);
+
+        var entries = new HashSet<PositionEntry>();
+
+        foreach (var line in lines)
+        {
+            var sanitizedLine = line.Replace("\t", string.Empty)
+                .Trim();
+            
+            var matches = MyRegex().IsMatch(sanitizedLine);
+
+            if (matches)
+            {
+                var sanitized = line.Replace(" ", string.Empty)
+                    .Replace("\t", string.Empty)
+                    .Replace("'", string.Empty);
+                var pieces = sanitized.Split("=");
+                var queue = new Queue<string>(pieces);
+
+                var entry = new PositionEntry(queue.Dequeue(), queue.Dequeue());
+                
+                entries.Add(entry);
+            }
+        }
+
+        var vectorArray = PathFinding.GetShortestPath(
+            startPosition, 
+            entries.Select(x => x.Position).ToArray()
+        );
+
+        var orderedEntries = new List<PositionEntry>();
+
+        foreach (var v in vectorArray)
+        {
+            // will exclude the origin point
+            if (!entries.Contains(new PositionEntry(v)))
+            {
+                continue;
+            }
+            
+            var entry = entries.Single(x => x == v);
+            orderedEntries.Add(entry);
+            
+            Console.WriteLine(entry.ToString());
+        }
+    }
+
+    [GeneratedRegex("^[a0-z9_]+?\\s?=\\s?'(.*?)',")]
+    private static partial Regex MyRegex();
+
+    public readonly struct PositionEntry
+    {
+        public string Name { get; }
+        public Vector3 Position { get; }
+
+        public PositionEntry(string name, string position)
+        {
+            Name = name;
+            Position = position.PositionToVector3();
+        }
+
+        public PositionEntry(Vector3 position)
+        {
+            Name = string.Empty;
+            Position = position;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} = '{Position.Vector3ToPosition()}',";
+        }
+
+        public bool Equals(PositionEntry other)
+        {
+            return Position.Equals(other.Position);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PositionEntry other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Position.GetHashCode();
+        }
+
+        public static bool operator ==(PositionEntry left, PositionEntry right)
+        {
+            return left.Equals(right);
+        }
+        
+        public static bool operator !=(PositionEntry left, PositionEntry right)
+        {
+            return !(left == right);
+        }
+        
+        public static bool operator ==(PositionEntry left, Vector3 right)
+        {
+            return left.Equals(new PositionEntry(right));
+        }
+        
+        public static bool operator !=(PositionEntry left, Vector3 right)
+        {
+            return !(left == right);
+        }
+    }
+}
